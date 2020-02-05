0c8f1bbce08d8b7daefc9300747067c141d9a214
Syifa Nurzain <elzainsyifa@gmail.com>
2020-02-05 10:49:43 +0700

    initial commit

---
+++ build/precache-manifest.9da1586f701c4507de170590abae8d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02f2d485734c0fb2edf327cb47b856a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "905846412db424065ec8",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad8e8dccf92391a62d32",
+    "url": "/portfolio/static/js/2.50fdd86e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.50fdd86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "905846412db424065ec8",
+    "url": "/portfolio/static/js/main.d7e0627a.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
