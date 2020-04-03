0b812e8f01030898e8a4c7157c3f714b5000bbed
Syifa Nurzain <elzainsyifa@gmail.com>
2020-04-03 20:56:49 +0700

    change header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Bg.css';
+import './Header.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Syifa Nurzain's Portfolio
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      animationClass: 'test',
+      text: "Hi, i'm Zain. A mobile development and UI/UX enthusiast.",
+      print: ''
+    }
+  }
+
+  componentWillMount () {
+    this.writeText()
+  }
+
+  writeText () {
+    const formatedText = this.state.text.split('')
+    let print = ''
+
+    const loop = setInterval(() => {
+      if (formatedText.length >= 1) {
+        if (formatedText[0] === '1') {
+          print += '<mark>'
+          formatedText.shift()
+        } else if (formatedText[0] === '2') {
+          print += '</mark>'
+          formatedText.shift()
+        } else {
+          print += formatedText.shift()
+          this.setState({ print })
+        }
+      } else { clearInterval(loop) }
+    }, 90)
+  }
+  
+  render() {
+    return (
+      <div style={{flexDirection: 'column'}}>
+        <div className={this.state.animationClass}>
+          <div className='typing-text'>
+            <h2 dangerouslySetInnerHTML={{__html: this.state.print}} />
+          </div>
+        </div>
+        <div>
+          <div className='container-footer'>
+            <p>About Me</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
