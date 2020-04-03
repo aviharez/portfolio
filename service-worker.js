7b07dc924b71d029b3c27d5e8869ec756d143475
Syifa Nurzain <elzainsyifa@gmail.com>
2020-04-03 20:58:24 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9da1586f701c4507de170590abae8d1a.js"
+  "/portfolio/precache-manifest.a4ab1a78f37a9a75fd2653323129e682.js"
 );
 
 self.addEventListener('message', (event) => {
